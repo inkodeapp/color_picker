9a34b4e3af4269dddde385d1688812e7e7d479e7
inkodeapp <53095543+inkodeapp@users.noreply.github.com>
2021-07-28 08:19:17 -0300

    Adición de cuadro de colores
    Agregar cuadro de color.

--- js/index.js
+++ js/index.js
@@ -3,4 +3,5 @@ const salida = document.querySelector("#salida");
 color.addEventListener("input", () => {
   const seleccion = color.value;
   salida.innerHTML = seleccion;
+  salida.style.background=seleccion;
 });
