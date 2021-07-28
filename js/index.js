7b6bc6a53d3ce7b4fc70ee95908424880cbe2cdc
inkodeapp <53095543+inkodeapp@users.noreply.github.com>
2021-07-28 14:10:18 -0300

    Adición de carcacteriscitas
    Favicon
    Background color en CSS

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 const color = document.querySelector("#color");
 const salida = document.querySelector("#salida");
+
 color.addEventListener("input", () => {
   const seleccion = color.value;
+
   salida.innerHTML = seleccion;
-  salida.style.background=seleccion;
+  salida.style.background = seleccion;
+  salida.style.color = "#ffffff";
 });
