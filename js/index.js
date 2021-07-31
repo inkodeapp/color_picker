273943225f8fb03abf4bce4ae30d8abe88685811
inkodeapp <53095543+inkodeapp@users.noreply.github.com>
2021-07-31 14:51:51 -0300

    Add
    Local Storage

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 const color = document.querySelector("#color");
 const salida = document.querySelector("#salida");
+const colorStorage = localStorage.getItem("color") || "#FFFFFF";
+salida.style.background = colorStorage;
+salida.innerHTML = colorStorage;
 
+console.log(localStorage);
 color.addEventListener("input", () => {
   const seleccion = color.value;
 
   salida.innerHTML = seleccion;
   salida.style.background = seleccion;
   salida.style.color = "#ffffff";
+  localStorage.clear();
+  localStorage.setItem("color", seleccion);
 });
